d31400eee8d4065278b7406feb542cacac54d765
陈文生 <2771628927@qq.com>
2022-05-03 12:48:47 +0800

    已优化: 并发情况下重复爬取问题 待优化: cookie的更新

--- src/main/java/org/cwsya/hifiadmin/service/reptile/AsyncData.java
+++ src/main/java/org/cwsya/hifiadmin/service/reptile/AsyncData.java
@@ -1,33 +1,39 @@
 package org.cwsya.hifiadmin.service.reptile;
 
 import cn.hutool.core.util.StrUtil;
+import org.aspectj.weaver.ast.Or;
 import org.cwsya.hifiadmin.mapper.MDataMapper;
 import org.cwsya.hifiadmin.pojo.PO.MDataEntity;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AsyncData {
 
+    private final RedisTemplate<String,String> redisTemplate;
 
     private final MDataMapper mDataMapper;
+    private final String redisDataName="music:data:";
 
-    public AsyncData(MDataMapper mDataMapper) {
+    public AsyncData(RedisTemplate<String, String> redisTemplate, MDataMapper mDataMapper) {
+        this.redisTemplate = redisTemplate;
         this.mDataMapper = mDataMapper;
     }
 
     @Async("msgThreadPool")
     public void reptileData(MDataEntity mDataEntity){
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
         if (mDataEntity.getUsable()!=1){
+            String key = redisDataName + mDataEntity.getSource() + ":" + mDataEntity.getUrl();
+            if (!StrUtil.isBlankIfStr(redisTemplate.opsForValue().get(key))){
+                System.out.println("已提交---->"+mDataEntity.getUrl());
+                return;
+            }
+            redisTemplate.opsForValue().set(redisDataName+mDataEntity.getSource()+mDataEntity.getUrl(),"1");
             String data = ReptileFactory.create(mDataEntity.getSource()).getData(mDataEntity.getUrl());
             if (!StrUtil.isBlankIfStr(data)) {
                 mDataMapper.updateById(new MDataEntity(mDataEntity.getId(),null,null,null,1,data));
+                redisTemplate.delete(key);
             }
         }
     }
